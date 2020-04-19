4da6e8f3a6ef1def8356244ee3014ff90719865a
hasegayo <y.hasegawa@karbit.co.jp>
2020-04-19 16:23:16 +0900

    feat: add GA tracking id(#7)

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-163988590-1`,
       },
     },
     {
